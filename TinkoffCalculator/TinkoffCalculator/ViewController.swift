2344de9f2ab3dea31f2d5bbe978284825193147e
onekir <kirichenkomap@gmail.com>
2024-01-29 16:59:53 +0100

    added the project

---
+++ TinkoffCalculator/TinkoffCalculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.titleLabel?.text else { return }
+        print(buttonText)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Start")
+    }
+
+
+}
+

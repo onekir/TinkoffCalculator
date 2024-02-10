6771ad0d631b6fed5161041f42352227bb447bcd
onekir <kirichenkomap@gmail.com>
2024-02-10 13:04:15 +0100

    Added result label, set constraints

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -9,8 +9,13 @@ class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         guard let buttonText = sender.currentTitle else { return }
+        label.text = buttonText
         print(buttonText)
     }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
